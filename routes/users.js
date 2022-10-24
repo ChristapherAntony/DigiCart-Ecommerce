a01af91488f7f05579c77544d1d788a445146453
Christapher Antony <christapher012@gmail.com>
2022-10-25 01:23:31 +0530

    dummy roots-done

--- routes/users.js
+++ routes/users.js
@@ -10,6 +10,26 @@ router.get('/viewAll', function(req, res, next) {
   res.render('users/user-viewAll');
 });
 
+router.get('/details', function(req, res, next) {
+  res.render('users/product-details');
+});
+
+router.get('/cart', (req, res, next)=> {
+  res.render('users/cart');
+});
+
+router.get('/wishlist', (req, res, next)=> {
+  res.render('users/wishlist');
+});
+
+router.get('/login-register', (req, res, next)=> {
+  res.render('users/login-signUp');
+});
+router.get('/account', (req, res, next)=> {
+  res.render('users/account');
+});
+ 
+
 
 
 module.exports = router;
