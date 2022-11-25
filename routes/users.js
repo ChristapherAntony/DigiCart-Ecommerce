12604e6fbb12c16455bbd6cb26a3d1d6e09c0287
Christapher Antony <christapher012@gmail.com>
2022-11-25 10:31:19 +0530

    latest

--- routes/users.js
+++ routes/users.js
@@ -441,8 +441,10 @@ router.post('/placeOrder', verifyUser, async (req, res) => {
           "payment_method": "paypal"
         },
         "redirect_urls": {
-          "return_url":process.env.PAYPAL_SUCCESS_URL+orderId,
-          "cancel_url":process.env.PAYPAL_FAIL_URL+orderId
+          // "return_url":process.env.PAYPAL_SUCCESS_URL+orderId,
+          // "cancel_url":process.env.PAYPAL_FAIL_URL+orderId
+          "return_url":"www.digicart.gq/orderSuccess/"+orderId,
+          "cancel_url":"http://digicart.gq/orderSuccess/"+orderId
         },
         "transactions": [{
           "amount": {
